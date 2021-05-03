832b1721d0b9ac3e315aa410928ad5c2f3125fca
JaiBoP <58470140+JaiBoP@users.noreply.github.com>
2021-05-03 22:16:31 +0400

    - Making code cleaner
    Project duplicated in folder to try and clean up code using a PyQt5.uic to load the UI info from the .ui file instead of 100+ lines of boilerplate code.

---
+++ reworked-version/main.py
@@ -0,0 +1,10 @@
+from frame import *
+import sys
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    MainWindow = QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
