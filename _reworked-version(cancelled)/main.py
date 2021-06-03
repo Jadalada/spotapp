a6eabaaea1483fca2125153ede2b90b0193e4e6f
JaiBoP <58470140+JaiBoP@users.noreply.github.com>
2021-06-03 09:05:29 +0400

    ^_^

--- _reworked-version(cancelled)/main.py
+++
@@ -1,9 +0,0 @@
-if __name__ == "__main__":
-    from frame import *
-    import sys
-    app = QApplication(sys.argv)
-    MainWindow = QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
-    sys.exit(app.exec_())
